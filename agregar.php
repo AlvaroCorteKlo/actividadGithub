6753f80f68385efb9f579c40594a01dc843c7063
Rushirufuruu <vicente.zapatasilva@gmail.com>
2024-05-02 16:08:00 -0400

    aplicacion funcional

---
+++ agregar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Agregar Nueva Tarea</title>
+</head>
+<body>
+
+<h1>Agregar Nueva Tarea</h1>
+
+<form action="agregar_tarea.php" method="post">
+    <label for="tarea">Nueva Tarea:</label><br>
+    <input type="text" id="tarea" name="tarea"><br>
+    <button type="submit">Agregar Tarea</button>
+</form>
+
+<a href="index.php">Volver a la lista de tareas</a>
+
+</body>
+</html>
